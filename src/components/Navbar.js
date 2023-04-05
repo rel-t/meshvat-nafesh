bfc586a4330eb2cacede2b62d0b19bce7df39c87
rel <arieltanami122@gmail.com>
2023-04-05 17:37:34 +0300

    Add navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,30 @@
-import './Navbar.css'
+import './Navbar.css';
 
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className='header'>
+    <div className="header">
+      {/* Logo */}
       <Link to={'/'}>
         <h1>משיבת נפש</h1>
       </Link>
-    </div>
-  )
-}
 
-export default Navbar
+      {/* Nav Links */}
+      <ul className='nav-menu'>
+        <li>
+          <Link to={'/about'}>אודות</Link>
+        </li>
+        <li>
+          <Link to={'/contact'}>צור קשר</Link>
+        </li>
+        <li>
+          <Link to={'/donation'}>תרום עכשיו</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
