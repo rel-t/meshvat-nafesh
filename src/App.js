febbf2d0f039af22d7da0e388fe4ea90895c020b
rel <arieltanami122@gmail.com>
2023-04-05 17:26:32 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Home from './routes/Home';
+import About from './routes/About';
+import Contact from './routes/Contact';
+import Donation from './routes/Donation';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Donation" element={<Donation />} />
+      </Routes>
+    </>
   );
 }
-
 export default App;
