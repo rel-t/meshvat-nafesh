db2b1fa2b3048904ea3e46ccc8a87bbc66101667
rel <arieltanami122@gmail.com>
2023-04-05 17:31:41 +0300

    Add Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import './Navbar.css'
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='header'>
+      <Link to={'/'}>
+        <h1>משיבת נפש</h1>
+      </Link>
+    </div>
+  )
+}
+
+export default Navbar
