82635f78d738c4f12c339fe1cf46353c58efdeb1
rel <arieltanami122@gmail.com>
2023-04-07 16:49:06 +0300

    Add HeroImg2 to About, Contact, and Donation page

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import HeroImg2 from '../components/HeroImg2';
 
 const About = () => {
+  
   return (
     <div>
       <Navbar />
+      <HeroImg2 heading="אודות" text="טקסט פסקה אודות"/>
       <Footer />
     </div>
   );
--- src/routes/Donation.js
+++ src/routes/Donation.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import HeroImg2 from '../components/HeroImg2';
 
 const Donation = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg2 heading="תרום עכשיו" text="טקסט פסקה תרומה בלה בלה.."/>
       <Footer />
     </div>
   );
